5e82131597893f6e54d822775ec2594b1c4770aa
st0n1e <cis.verbruggen@student.kdg.be>
2023-12-07 11:01:58 +0100

    Added modkey+w firefox shortkey

--- config.def.h
+++ config.def.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *firefox[] = { "firefox", NULL }
+static const char *firefox[] = { "firefox" };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
--- config.h
+++ config.h
@@ -58,11 +58,13 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
+static const char *firefox[] = { "firefox" };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,         			    XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,         			    XK_w,	   spawn,          {.v = firefox } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
